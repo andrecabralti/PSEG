0092a48310f967dffa949ab87a447e72142890aa
Andre Cabral <130406384+andrecabralti@users.noreply.github.com>
2023-07-15 13:36:37 -0300

    versão1

---
+++ script.js
@@ -0,0 +1,41 @@
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+
+links.forEach(ativarLink);
+
+const button = document.querySelector(".btn-contato");
+const modal = document.querySelector("dialog");
+const buttonClose = document.querySelector("dialog button");
+
+button.onclick = function() {
+  modal.showModal();
+}
+
+buttonClose.onclick = function () {
+  modal.close()
+}
+
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
